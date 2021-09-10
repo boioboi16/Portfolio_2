c8f3c8f3a0b63b3f957ea3c2cc94fda71db4e708
boioboi16 <aaryan.praveen@gmail.com>
2021-09-10 16:15:35 +0530

    content height changed

--- app.js
+++ app.js
@@ -38,3 +38,38 @@ hamburger.addEventListener('click',()=>{
 
 // })
 
+// new Glider(document.querySelector('.glider'), {
+//   slidesToShow: 1,
+//   slidesToScroll: 1,
+//   draggable: true,
+//   // dots: '.dots',
+//   arrows: {
+//     prev: '.glider-prev',
+//     next: '.glider-next'
+//   }
+// });
+
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:70,
+  nav:false,
+  stagePadding:0,
+  // lazyLoad:true,
+  autoWidth:true,
+  center:true,
+  // itemMargin:20,
+  responsive:{
+      0:{
+          items:1
+      },
+      // 400:{
+      //   itmes:2
+      // },
+      600:{
+          items:2
+      },
+      1000:{
+          items:3
+      }
+  }
+})
